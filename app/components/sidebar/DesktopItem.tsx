6fdd3466537b4a9d90641caa402744d89f0be69f
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-05-17 10:56:28 -0400

    added conversations

--- app/components/sidebar/DesktopItem.tsx
+++ app/components/sidebar/DesktopItem.tsx
@@ -1,42 +1,52 @@
 import clsx from 'clsx';
-import Link from 'next/link';
+import Link from "next/link";
 
 interface DesktopItemProps {
   label: string;
-  icon: React.ElementType;
-  href: string | undefined;
-  active: boolean | undefined;
+  icon: any;
+  href: string;
   onClick?: () => void;
+  active?: boolean;
 }
 
-const DesktopItem: React.FC<DesktopItemProps> = ({
-  label,
-  icon: Icon,
-  href,
+const DesktopItem: React.FC<DesktopItemProps> = ({ 
+  label, 
+  href, 
+  icon: Icon, 
   active,
-  onClick,
+  onClick
 }) => {
-
   const handleClick = () => {
     if (onClick) {
       return onClick();
     }
   };
 
-  return (
-    <li onClick={handleClick}>
+  return ( 
+    <li onClick={handleClick} key={label}>
       <Link
-        href={''}
-        className={clsx(
-          `group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100`,
-          active && 'bg-gray-100 text-black'
-        )}
+        href={href}
+        className={clsx(`
+            group 
+            flex 
+            gap-x-3 
+            rounded-md 
+            p-3 
+            text-sm 
+            leading-6 
+            font-semibold 
+            text-gray-500 
+            hover:text-black 
+            hover:bg-gray-100
+          `,
+            active && 'bg-gray-100 text-black'
+          )}
       >
-        <Icon className="h-6 w-6 shrink-0" />
-        <span className='sr-only'>{label}</span>
+        <Icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+        <span className="sr-only">{label}</span>
       </Link>
     </li>
-  );
-};
-
+   );
+}
+ 
 export default DesktopItem;
--- app/hooks/useRoutes.ts
+++ app/hooks/useRoutes.ts
@@ -1,35 +1,36 @@
 import { useMemo } from "react";
 import { usePathname } from "next/navigation";
-import { HiChat } from 'react-icons/hi'
-import { HiArrowLeftOnRectangle, HiUsers } from 'react-icons/hi2'
+import { HiChat } from 'react-icons/hi';
+import { HiArrowLeftOnRectangle, HiUsers } from 'react-icons/hi2';
 import { signOut } from "next-auth/react";
 import useConversation from "./useConversation";
 
 const useRoutes = () => {
-    const pathNmae = usePathname()
-    const { conversationId } = useConversation()
-    const routes = useMemo(() => {
-        return [
-            {
-                href: '/conversations',
-                label: 'Chat',
-                icon: HiChat,
-                active: pathNmae === '/conversations' || !!conversationId
-            },
-            {
-                href: '/users',
-                label: 'Users',
-                icon: HiUsers,
-                active: pathNmae === '/users'
-            },
-            {
-                path: '#',
-                label: 'Sign Out',
-                icon: HiArrowLeftOnRectangle,
-                onClick: () => signOut()
-            }
-        ]
-    },[pathNmae, conversationId])
-    return routes
-}
-export default useRoutes
+  const pathname = usePathname();
+  const { conversationId } = useConversation();
+
+  const routes = useMemo(() => [
+    { 
+      label: 'Chat', 
+      href: '/conversations', 
+      icon: HiChat,
+      active: pathname === '/conversations' || !!conversationId
+    },
+    { 
+      label: 'Users', 
+      href: '/users', 
+      icon: HiUsers, 
+      active: pathname === '/users'
+    },
+    {
+      label: 'Logout', 
+      onClick: () => signOut(),
+      href: '#',
+      icon: HiArrowLeftOnRectangle, 
+    }
+  ], [pathname, conversationId]);
+
+  return routes;
+};
+
+export default useRoutes;
