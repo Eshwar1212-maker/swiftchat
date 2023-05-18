7ec52c18488609efaaa6f5501dc7441acf7cadc4
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-05-18 15:04:50 -0400

    created getMessages action

--- app/actions/getConversationById.ts
+++ app/actions/getConversationById.ts
@@ -1,18 +1,24 @@
 import prisma from "@/app/libs/prismadb"
 import getCurrentUser from "./getCurrentUser"
 
-const getConversationById = async(
+const getConversationById = async (
     conversationId: string
 ) => {
-    try{
+    try {
         const currentUser = await getCurrentUser()
-        if(!currentUser?.email) return null
+        if (!currentUser?.email) return null
         const conversation = await prisma.conversation.findUnique({
             where: {
                 id: conversationId
+            },
+            include: {
+                users: true
             }
         })
-    }catch(err){
+        return conversation
+    } catch (err) {
         return null
     }
 }
+
+export default getConversationById; 
