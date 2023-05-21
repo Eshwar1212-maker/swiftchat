88472bd0d239f756a36da38c71103d53a09eddc0
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-05-21 13:46:05 -0400

    adding group chats

--- app/components/inputs/Select.tsx
+++ app/components/inputs/Select.tsx
@@ -1,4 +1,5 @@
 "use client"
+import ReactSelect from "react-select"
 
 interface SelectProps{
     label: string
@@ -19,6 +20,27 @@ const Select: React.FC<SelectProps> = ({
         <label className="block text-sm font-medium leading-6 text-gray-900">
             {label}
         </label>
+        <div className="mt-2">
+            <ReactSelect
+                isDisabled={disabled}
+                value={value}
+                onChange={onChange}
+                isMulti
+                options={options}
+                menuPortalTarget={document.body}
+                styles={{
+                    menuPortal: (base) => ({
+                        ...base,
+                        zIndex: 9999
+                    })
+                }}
+                classNames={{
+                    control: () => "text-sm"
+                }}
+            />
+
+            
+        </div>
     </div>
   )
 }
