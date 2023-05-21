38d5bfe0fc86309d0025f9f499a188567c593e6f
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-05-21 13:29:54 -0400

    adding group chats

--- app/components/inputs/Select.tsx
+++ app/components/inputs/Select.tsx
@@ -1,10 +1,20 @@
 "use client"
 
+interface SelectProps{
+    label: string
+    value?: Record<string, any>
+    onChange: (value: Record<string, any>) => void
+    options: Record<string, any>[]
+    disabled?: boolean
+}
 
-const Select = () => {
+
+const Select: React.FC<SelectProps> = ({
+
+}) => {
   return (
     <div>
-        
+
     </div>
   )
 }
