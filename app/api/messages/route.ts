2dc1898abffd485711a51d288e172630924e029b
Eshwar Tangirala <eshwartangirala11@gmail.com>
2023-05-23 12:07:20 -0400

    adding realtime communication with pusher

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,9 +1,6 @@
 import { NextResponse } from "next/server";
 import { pusherServer } from "@/app/libs/pusher";
-
-
 import getCurrentUser from "@/app/actions/getCurrentUser";
-// import { pusherServer } from '@/app/libs/pusher'
 import prisma from "@/app/libs/prismadb";
 
 export async function POST(
@@ -17,11 +14,9 @@ export async function POST(
       image,
       conversationId
     } = body;
-
     if (!currentUser?.id || !currentUser?.email) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
-
     const newMessage = await prisma.message.create({
       include: {
         seen: true,
@@ -43,8 +38,6 @@ export async function POST(
         },
       }
     });
-
-    
     const updatedConversation = await prisma.conversation.update({
       where: {
         id: conversationId
@@ -66,18 +59,14 @@ export async function POST(
         }
       }
     });
-
     await pusherServer.trigger(conversationId, 'messages:new', newMessage);
-    
     const lastMessage = updatedConversation.messages[updatedConversation.messages.length - 1];
-
     updatedConversation.users.map((user) => {
       pusherServer.trigger(user.email!, 'conversation:update', {
         id: conversationId,
         messages: [lastMessage]
       });
     });
-
     return NextResponse.json(newMessage)
   } catch (error) {
     console.log(error, 'ERROR_MESSAGES')
